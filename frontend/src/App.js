a99931695f7dd5665575c36ee02fc2fc5a0fa3e4
adityababar07 <adityababar715@gmail.com>
2021-05-15 18:32:13 +0000

    Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Switch, Route} from 'react-router-dom';
 import Navbar from "./navbar/Navbar";
-import Home from "./Home";
+import Home from "./home/Home";
 import './App.css';
 
 function App() {
